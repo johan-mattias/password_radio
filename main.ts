49a022ab25d29c576eba9f0d0e947874d050baa0
Johan <3921204+johan-mattias@users.noreply.github.com>
2020-11-25 15:32:11 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,15 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendString("1234qwer")
-    basic.showIcon(IconNames.Chessboard)
-    basic.clearScreen()
-})
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    
+radio.onReceivedString(function (receivedString) {
     count += 1
     if (receivedString == password) {
         radio.sendString("pass")
         basic.showIcon(IconNames.Square)
-    } else if (receivedString == "pass") {
-        basic.showIcon(IconNames.Target)
     }
-    
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     basic.showNumber(count)
 })
 let count = 0
 let password = ""
-password = "1234qwer"
+password = "x"
 count = 0
+basic.showIcon(IconNames.Target)
